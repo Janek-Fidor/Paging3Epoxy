e6762247500fbd473e12ea1375915d7720909263
janfidor <jfidor@airnauts.com>
2022-08-26 15:45:43 +0200

    update sources of data

--- domain/src/main/java/com/example/domain/feature/pokemon/PokemonRemoteDataSource.kt
+++ domain/src/main/java/com/example/domain/feature/pokemon/PokemonRemoteDataSource.kt
@@ -1,9 +1,8 @@
 package com.example.domain.feature.pokemon
 
-import androidx.paging.PagingData
 import com.example.domain.core.ApiResult
-import kotlinx.coroutines.flow.Flow
 
 interface PokemonRemoteDataSource {
-    suspend fun getById(id: Int): ApiResult<Pokemon>
+    suspend fun getPokemonById(id: Int): ApiResult<Pokemon>
+
 }
