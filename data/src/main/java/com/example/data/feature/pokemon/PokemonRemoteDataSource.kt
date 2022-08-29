ba13724c028f1196c935034ee46236c61af90963
janfidor <jfidor@airnauts.com>
2022-08-29 13:28:42 +0200

    expand DataSource interface with get page method

--- data/src/main/java/com/example/data/feature/pokemon/PokemonRemoteDataSource.kt
+++ data/src/main/java/com/example/data/feature/pokemon/PokemonRemoteDataSource.kt
@@ -1,7 +1,11 @@
 package com.example.data.feature.pokemon
 
+import com.example.data.core.ApiResult
 import com.example.data.feature.pokemon.models.NetworkPokemon
+import com.example.data.feature.pokemon.models.NetworkPokemonPage
 
 interface PokemonRemoteDataSource {
-    suspend fun getPokemonById(id: Int): Result<NetworkPokemon>
+    suspend fun getPokemonById(id: Int): ApiResult<NetworkPokemon>
+
+    suspend fun getPokemonPageById(id: Int): ApiResult<NetworkPokemonPage>
 }
