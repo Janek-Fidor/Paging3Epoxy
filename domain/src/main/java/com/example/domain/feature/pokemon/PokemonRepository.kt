cdb41dc0f49079d2d98de241b286bcc0cbd14ba5
janfidor <jfidor@airnauts.com>
2022-08-29 10:08:49 +0200

    update repository and paging source

--- domain/src/main/java/com/example/domain/feature/pokemon/PokemonRepository.kt
+++ domain/src/main/java/com/example/domain/feature/pokemon/PokemonRepository.kt
@@ -1,8 +1,9 @@
 package com.example.domain.feature.pokemon
 
 import androidx.paging.PagingData
+import com.example.domain.core.ApiResult
 import kotlinx.coroutines.flow.Flow
 
 interface PokemonRepository {
-    fun getPokemons(): Flow<PagingData<Pokemon>>
+    suspend fun getPokemonPageById(id: Int): ApiResult<PokemonPage>
 }
