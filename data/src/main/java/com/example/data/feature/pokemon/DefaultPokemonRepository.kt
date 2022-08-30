fc239cc2a0637c57e54a0e6ed46e513a4d86c489
janfidor <jfidor@airnauts.com>
2022-08-30 12:28:47 +0200

    add DefaultPokemonRepository

---
+++ data/src/main/java/com/example/data/feature/pokemon/DefaultPokemonRepository.kt
@@ -0,0 +1,25 @@
+package com.example.data.feature.pokemon
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.domain.feature.pokemon.PokemonRepository
+import com.example.domain.feature.pokemon.models.PokemonSnapshot
+import kotlinx.coroutines.flow.Flow
+
+private const val VISIBLE_PAGE_SIZE = 20
+private const val PAGE_SIZE = VISIBLE_PAGE_SIZE * 2
+private const val PREFETCH_DISTANCE = PAGE_SIZE * 2
+
+class DefaultPokemonRepository(private val dataSource: PokemonRemoteDataSource) :
+    PokemonRepository {
+    override fun fetchPokemonPages(): Flow<PagingData<PokemonSnapshot>> = Pager(
+        pagingSourceFactory = { PokemonPagingSource(dataSource) },
+        config = PagingConfig(
+            pageSize = PAGE_SIZE,
+            prefetchDistance = PREFETCH_DISTANCE,
+            enablePlaceholders = false,
+            initialLoadSize = PAGE_SIZE
+        )
+    ).flow
+}
