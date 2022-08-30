4bb1721b3a4e0cf91b51db81f11b4fb68f63b749
janfidor <jfidor@airnauts.com>
2022-08-30 12:29:10 +0200

    add logging interceptor to DefaultPokemonRemoteDataSource

--- data/src/main/java/com/example/data/feature/pokemon/DefaultPokemonRemoteDataSource.kt
+++ data/src/main/java/com/example/data/feature/pokemon/DefaultPokemonRemoteDataSource.kt
@@ -7,16 +7,27 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 @OptIn(ExperimentalSerializationApi::class)
 class DefaultPokemonRemoteDataSource : PokemonRemoteDataSource {
-    private val contentType: MediaType = MediaType.get("application/json")
     private val url = "https://pokeapi.co/api/v2/"
+
+    private val interceptor = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+    private val contentType: MediaType = "application/json".toMediaType()
+    private val json = Json { ignoreUnknownKeys = true }
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(url)
-        .addConverterFactory(Json.asConverterFactory(contentType))
+        .addConverterFactory(json.asConverterFactory(contentType))
+        .client(client)
         .build()
 
     private val pokemonService by lazy { retrofit.create(PokemonApiService::class.java) }
