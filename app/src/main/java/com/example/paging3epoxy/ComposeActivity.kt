fb0c42d30b47bdd82ca093a3d0bb93e98734c51f
janfidor <jfidor@airnauts.com>
2022-08-31 11:53:55 +0200

    add compose + epoxy & pure compose paging lists

--- app/src/main/java/com/example/paging3epoxy/ComposeActivity.kt
+++ app/src/main/java/com/example/paging3epoxy/ComposeActivity.kt
@@ -3,41 +3,150 @@ package com.example.paging3epoxy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import androidx.core.view.setPadding
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import androidx.recyclerview.widget.LinearLayoutManager
+import coil.compose.AsyncImage
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.example.domain.feature.pokemon.models.Pokemon
 import com.example.paging3epoxy.ui.theme.Paging3EpoxyTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Paging3EpoxyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                PokemonListScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PokemonListScreen(
+    viewModel: PokemonViewModel = hiltViewModel()
+) {
+//    val pokemonPagingData = viewModel.pokemonFlow
+//    PokemonList(pokemonPagingData)
+
+    val pokemonPagingData = viewModel.pokemonFlow.collectAsLazyPagingItems()
+    PokemonListNotEpoxy(pokemonPagingData)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Paging3EpoxyTheme {
-        Greeting("Android")
+fun PokemonListNotEpoxy(pokemonPagingItems: LazyPagingItems<Pokemon>) {
+    LazyColumn {
+        items(pokemonPagingItems, { item: Pokemon -> item.id })
+        { pokemon ->
+            pokemon?.let { PokemonEntry(pokemon) }
+        }
     }
+}
+
+@Composable
+fun PokemonEntry(pokemon: Pokemon) {
+    ConstraintLayout(
+        Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+    ) {
+        val (details, image) = createRefs()
+
+        val pokemonDetails = PokemonDetailsUIState(
+            name = pokemon.name,
+            types = pokemon.types.joinToString(" "),
+            abilities = pokemon.abilities.joinToString(" ")
+        )
+
+        PokemonDetails(
+            pokemonDetails,
+            modifier = Modifier.constrainAs(details) {
+                start.linkTo(parent.start, margin = 16.dp)
+            }
+        )
+        PokemonImage(
+            pokemonImageUrl = pokemon.pictureUrl,
+            modifier = Modifier.constrainAs(image) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+
+                height = Dimension.fillToConstraints
+            },
+        )
+    }
+}
+
+data class PokemonDetailsUIState(
+    val name: String,
+    val types: String,
+    val abilities: String,
+)
+
+@Composable
+fun PokemonDetails(pokemonDetails: PokemonDetailsUIState, modifier: Modifier = Modifier) {
+    Surface(modifier = modifier) {
+        Column() {
+            Text(text = pokemonDetails.name, style = typography.h1)
+            Text(text = pokemonDetails.types, style = typography.body1)
+            Text(text = pokemonDetails.abilities, style = typography.body1)
+        }
+    }
+}
+
+@Composable
+fun PokemonImage(pokemonImageUrl: String, modifier: Modifier = Modifier) {
+    AsyncImage(
+        modifier = modifier,
+        model = pokemonImageUrl,
+        contentDescription = "pokemon image",
+        contentScale = ContentScale.FillHeight,
+    )
+}
+
+
+@Composable
+fun PokemonList(pokemonPagingData: Flow<PagingData<Pokemon>>) {
+    val coroutineScope = rememberCoroutineScope()
+
+    AndroidView(factory = { ctx ->
+        val epoxyController = PokemonListEpoxyController()
+        val view = EpoxyRecyclerView(ctx).apply {
+            setController(epoxyController)
+            setItemSpacingDp(8)
+            setPadding(8)
+            layoutManager = LinearLayoutManager(ctx)
+        }
+
+        coroutineScope.launch {
+            pokemonPagingData.collect {
+                epoxyController.submitData(it)
+            }
+        }
+        view
+    }, modifier = Modifier.fillMaxWidth())
+
 }
--- app/src/main/java/com/example/paging3epoxy/ui/theme/Type.kt
+++ app/src/main/java/com/example/paging3epoxy/ui/theme/Type.kt
@@ -8,10 +8,16 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
+    ),
+
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 20.sp
     )
     /* Other default text styles to override
     button = TextStyle(
